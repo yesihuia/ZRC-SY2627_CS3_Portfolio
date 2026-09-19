2a06680cb5895c381fe4c28b1bc4b702ffb691d3
yeshua <svsigue@zrc.pshs.edu.ph>
2026-09-19 14:10:58 +0800

    Implementing Course and Student Classes

--- q1/sg6/real-life-relationship-mapping/main.py
+++ q1/sg6/real-life-relationship-mapping/main.py
@@ -1,1 +1,27 @@
-# Write a short Python code snippet showing a Course adding a Student object to a list. 
+class Student:
+    def __init__(self, name, student_id):
+        self.name = name
+        self.student_id = student_id
+
+
+class Course:
+    def __init__(self, name):
+        self.name = name
+        self.students = []
+
+    def add_student(self, student):
+        self.students.append(student)
+
+
+student1 = Student("Yeshua", "033")
+student2 = Student("Matthew", "079")
+
+course = Course("Computer Science")
+
+course.add_student(student1)
+course.add_student(student2)
+
+print(course.name)
+
+for student in course.students:
+    print(student.name)
